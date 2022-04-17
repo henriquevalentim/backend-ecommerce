3db11a990e2902531198170908910878f0a6346f
Henrique Valentim Nunes <henriquevalentim16@gmail.com>
2022-04-17 15:08:22 -0300

    config access server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: "http://localhost:3000",
 };
 
 require("dotenv").config({ path: path.join(__dirname, ".env") });
